f4d8c157e447e80531d89929abb844a747e477af
René Loos <mail@reneloos.nl>
2015-11-24 11:43:29 +0100

    Alfabetische volgorde plaatsnamen

--- layout/header.php
+++ layout/header.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
-    <title><?php if(isset($title)){ echo $title; }?></title>
+    <title>
+        <?php if(isset($title)){ echo $title; }?>
+    </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-select.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -14,31 +17,75 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script src="js/bootstrap-select.js"></script>
 
-<!--    Plaatsnamen voor homepage -->
-  <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-  <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-  <script>
-  $(function() {
-    var availableTags = [
+    <!--    Plaatsnamen voor homepage -->
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script>
+        $(function () {
+            var availableTags = [
+//      A
       "Amsterdam",
       "Alkmaar",
       "Almere",
+//      B
       "Beverwijk",
       "Brabant",
+//      C
+      "Castricum",
+//      D
       "Den Haag",
+//      E
+       "Ede",
+//      F
+       "Finkum",
+//      G
       "Groningen",
+//      H
+      "Haarlem",
+//      I
+      "IJsselmuiden",
+//      J
+      "Jisp",
+//      K
+      "Katwijk",
+//      L
+      "Leiden",
+//      M
+      "Maastricht",
+//      N
+      "Nunspeet",
+//      O
+      "Opmeer",
+      "Obdam",
+//      P
+      "Pieterburen",
+//      R
+      "Roermond",
       "Rotterdam",
+//      S
+      "Schagen",
+//      T
+      "Tilburg",
+//      U
       "Utrecht",
+//      V
+      "Volendam",
+//      W
+      "Waalwijk",
+//      Y
+      "Yerseke",
+//      Z
       "Zwolle"
     ];
-    $( "#tags" ).autocomplete({
-      source: availableTags
-    });
-  });
-</script>
+            $("#tags").autocomplete({
+                source: availableTags
+            });
+        });
+    </script>
 
 </head>
+
 <body>
     <!-- Navigation -->
     <div class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -67,51 +114,53 @@
 
     			foreach($userInfo as $info) {
                 ?>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown active">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <span class="glyphicon glyphicon-user"></span>
-                            <strong><?= $info['firstname']; ?></strong>
-                            <span class="glyphicon glyphicon-chevron-down"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <div class="navbar-login">
-                                    <div class="row">
-                                        <div class="col-lg-4">
-                                            <p class="text-center">
-                                                <img height="90" width="90" src="images/profile/<?= $info['image']; ?>">
-                                            </p>
-                                        </div>
-                                        <div class="col-lg-8">
-                                            <p class="text-left"><strong><?= $info['username']; ?></strong></p>
-                                            <p class="text-left small"><?= $info['email']; ?></p>
-                                            <p class="text-left">
-                                                <a href="memberpage.php" class="btn btn-primary btn-block btn-sm">Profile</a>
-                                            </p>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown active">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <span class="glyphicon glyphicon-user"></span>
+                                <strong><?= $info['firstname']; ?></strong>
+                                <span class="glyphicon glyphicon-chevron-down"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <div class="navbar-login">
+                                        <div class="row">
+                                            <div class="col-lg-4">
+                                                <p class="text-center">
+                                                    <img height="90" width="90" src="images/profile/<?= $info['image']; ?>">
+                                                </p>
+                                            </div>
+                                            <div class="col-lg-8">
+                                                <p class="text-left"><strong><?= $info['username']; ?></strong></p>
+                                                <p class="text-left small">
+                                                    <?= $info['email']; ?>
+                                                </p>
+                                                <p class="text-left">
+                                                    <a href="memberpage.php" class="btn btn-primary btn-block btn-sm">Profile</a>
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </li>
-                            <li class="divider navbar-login-session-bg"></li>
-                            <li><a href='edit.php'>Edit Profile <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
-                            <li><a href='addTour.php'>Add Tour <span class="glyphicon glyphicon-flag pull-right"></span></a></li>
-                            <?php if($info['username'] == "admin"){ ?>
-                            <li><a href='admin.php'>Admin panel <span class="glyphicon glyphicon-flag pull-right"></span></a></li>
-                            <?php } ?>
-                            <li><a href='logout.php'>Sign Out <span class="glyphicon glyphicon-log-out pull-right"></span></a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <?php
+                                </li>
+                                <li class="divider navbar-login-session-bg"></li>
+                                <li><a href='edit.php'>Edit Profile <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+                                <li><a href='addTour.php'>Add Tour <span class="glyphicon glyphicon-flag pull-right"></span></a></li>
+                                <?php if($info['username'] == "admin"){ ?>
+                                    <li><a href='admin.php'>Admin panel <span class="glyphicon glyphicon-flag pull-right"></span></a></li>
+                                    <?php } ?>
+                                        <li><a href='logout.php'>Sign Out <span class="glyphicon glyphicon-log-out pull-right"></span></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <?php
             }
         } else { ?>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="index.php?page=register">Register</a></li>
-                <li><a href="index.php?page=login">Login</a></li>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="index.php?page=register">Register</a></li>
+                            <li><a href="index.php?page=login">Login</a></li>
 
-            </ul>
-        <?php } ?>
+                        </ul>
+                        <?php } ?>
             </div>
         </div>
     </div>
